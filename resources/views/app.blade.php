ef5b0fa923d87f284d07f6f9bd5544de4ce81186
Amiqatun Nasyati Yusri <nasyati.miqa@gmail.com>
2025-10-17 23:39:18 +0800

    feat: add logo app

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -10,6 +10,9 @@
         <link rel="preconnect" href="https://fonts.bunny.net">
         <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
 
+        <!-- Logo -->
+        <link rel="icon" type="image/png" sizes="32x32" href="{{ asset('/Bulukumba_Regency_Logo.png') }}">
+
         <!-- Scripts -->
         @routes
         @viteReactRefresh
